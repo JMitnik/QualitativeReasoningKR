1f3ee57ace6a9b2e25335db0e6dd3b4cde5f4581
Jonathan Mitnik <JMitnik@gmail.com>
2019-04-02 22:19:41 +0200

    Refactor causal graphs

--- causal_graph.py
+++ causal_graph.py
@@ -13,7 +13,6 @@ class CausalGraph(object):
         relations_specs = specs['relations']
         init_vals = specs['init_state']
         
-        # print(self.entities)
         for rel_spec in relations_specs:
             print(rel_spec)
             rel = Relation(rel_spec['ty'], [rel_spec['from'], rel_spec['to']], rel_spec['args'])
@@ -26,13 +25,12 @@ class CausalGraph(object):
         for entity in entities_specs:
             d = Derivative(DerivativeSpace, DerivativeSpace(entity['d_value']))
             mag_space = mag_q_space[entity['mag_q_space']]
-            mag = Magnitude(mag_space, entity['mag_value'])
-            entities.append(Entity(entity['title'], mag, d))
+            mag = Magnitude(mag_space, mag_space(entity['mag_value']))
+            # Todo: for completeness, maybe we should just set the spec to have a list of quantities within an entity (even-though we only have one quantity per entity)
+            quantity = Quantity(entity['title'], mag, d)
+            entities.append(Entity(entity['title'], quantity))
 
         return entities
-
-    def _load_relations(relations_specs):
-
 
 if __name__ == "__main__":
     from causal_graph_specs import specs
