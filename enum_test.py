6e6242a4545367ddb8fd7f55d9c15d1180049d1f
Jonathan Mitnik <JMitnik@gmail.com>
2019-04-07 19:45:51 +0200

    IntEnum

--- enum_test.py
+++ enum_test.py
@@ -1,9 +1,12 @@
-from enum import Enum
+from enum import IntEnum
 
-class Pressure(Enum):
+class Pressure(IntEnum):
     ZERO = 1
     PLUS = 2
     MAX = 3
 
 if __name__ == "__main__":
-    p = Pressure.ZERO
+    p_zero = Pressure.ZERO
+    p_zero_2 = Pressure.ZERO
+    p_plus = Pressure.PLUS
+    print(p_plus)
