b9f7be8ee776aac4211a449c28903b6d11d27abd
Jonathan Mitnik <JMitnik@gmail.com>
2019-04-11 02:32:49 +0200

    Cleanup

--- causal_graph.py
+++ causal_graph.py
@@ -18,7 +18,6 @@ class CausalGraph:
         '''
         self.entities = entities
         self.relations = relations
-        # self.incoming_relation_map = {rel.to: rel for rel in relations}
         self.incoming_relation_map = defaultdict(list)
 
         for rel in relations:
@@ -47,10 +46,6 @@ class CausalGraph:
         return self._to_state(self.entities)
 
     def _load_state(self, state):
-        # The index of each namedtuple in the state corresponds to the entity in
-        # self.entities.
-
-        # Go over each entity, and set their value
         for index, entity in enumerate(state):
             self.entities[index].load_from_tuple(entity)
 
@@ -71,7 +66,6 @@ class CausalGraph:
             else:
                 result.append(relation_states)
 
-        # PIN: States aren't printed as we want yet.
         states = set(product(*result))
 
         return states
@@ -93,6 +87,7 @@ class CausalGraph:
         '''Discover new states from the given state
         '''
         self._load_state(state)
+        # TODO: Include exo variables
         deriv_states_set = self._apply_derivative_to_entities(self.entities)
         relation_states_set = self._apply_relations_to_entities(self.entities)
 
@@ -108,70 +103,6 @@ class CausalGraph:
         # Check that VC's are met!
         # Ensure we clip all of derivatives 
         pass
-
-    def propagate(self, state: tuple):
-        all_possible_states = []
-        def new_entities(): return deepcopy(self.entities)
-        # 1. check the ambiguity of exogenous variable
-
-        # TODO Make this a consistent Enum
-
-        exo_var_n = 'inlet'
-        exo_var = self.entities[exo_var_n]
-        # TODO: Make valid_der method
-        for valid_der in exo_var.quantity.valid_der():
-            new_s = new_entities()
-            new_exo_var = new_s[exo_var_n]
-
-            # TODO: Make set_der method
-            new_exo_var.set_der(valid_der)
-            all_possible_states.append(new_s)
-
-        # 2. check the ambiguity of derivative applying
-        state_li_after_applying = []
-
-        for entity_n in self.entities:
-            entity = self.entities[entity_n]
-            ent_li = entity.apply_der()
-            state_li_after_applying.append(ent_li)
-            # if entity.derivative!=0:
-            #     if entity.quantity==0:
-            #         new_s = new_entities()
-            #         new_s[entity_n].apply_der()
-        new_entities += list(product(state_li_after_applying))
-
-        # TODO: Create another loop which checks for exogenous states within possible ambigutiies (other than stable)
-
-        # 3. check the ambiguity of relations
-        for entity_n in self.relation_map:
-            rels = self.relation_map[entity_n]
-            rels_n = {r.ty: r for r in rels}
-            q_influence = []
-            d_influence = []
-
-            # {from: q1, to: q2, type: vc} , {}
-            for rel_n in rels_n:
-                rel = rels[rel_n]
-                if rel_n == 'I+':
-                    influ = self.entities[rel.fr].quantity.val * 1
-                    d_influence.append(influ)
-                if rel_n == 'I-':
-                    influ = self.entities[rel.fr].quantity.val * -1
-                    d_influence.append(influ)
-                if rel_n == 'P+':
-                    influ = self.entities[rel.fr].quantity.val * 1
-                    q_influence.append(influ)
-                if rel_n == 'P-':
-                    influ = self.entities[rel.fr].quantity.val * -1
-                    q_influence.append(influ)
-                if rel_n == 'VC':
-                    # TODO: Use entitiy instead of entity_n
-                    # TODO: Check if the 'from' enttity has reached the VC value as well
-                    self.entities[rel.to].quantity.val = self.entities[rel.fr].quantity.val
-                    break
-
-            # If q and d influence have conflicts, generate new states and append
-
 
 if __name__ == "__main__":
     pass
